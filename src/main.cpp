624b622aafdf4c9b8bfbd633ecdf2ff4b6de647e
Augusto Mota Pinheiro <augustomp55@gmail.com>
2025-01-06 15:47:34 -0500

    fix: updated encoder instantiation

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ void task_encoder(void *)
   const uint32_t PIN_B = 23;
   const int16_t PPR = 512;
 
-  Encoder encoder(PIN_A, PIN_B, PPR);
+  newton::Encoder encoder(PIN_A, PIN_B, PPR);
 
   while (true)
   {
@@ -31,7 +31,7 @@ void task_encoder(void *)
     float position = encoder.get_position();
     float velocity = encoder.get_velocity();
 
-    // printf("%f, %f\n", position, velocity);
+    printf("%f, %f\n", position, velocity);
   }
 }
 
@@ -41,7 +41,7 @@ void task_actuator(void *)
   ESP_ERROR_CHECK(esp_task_wdt_add(NULL));
   ESP_ERROR_CHECK(esp_task_wdt_status(NULL));
 
-  const uint32_t PIN_PWM = 12;
+  const uint32_t PIN_PWM = 14;
   const uint16_t MIN_PULSE_WIDTH = 1100;
   const uint16_t MAX_PULSE_WIDTH = 1900;
 
