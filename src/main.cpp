3d61265290475d796312fd5f469a844dcf25ef41
Augusto Mota Pinheiro <augustomp55@gmail.com>
2025-01-05 23:25:18 -0500

    feat: added working encoder class + separated newton_actuator into a repo

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,40 @@
 #include <stdio.h>
 
-#include "encoder.h"
+#include "newton/encoder.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <esp_log.h>
+#include <esp_err.h>
+#include "esp_task_wdt.h"
 
 const uint32_t PIN_A = 22;
 const uint32_t PIN_B = 23;
-const int16_t CPR = 512;
+const int16_t CPR = 2048;
 
-extern "C" void app_main() {
+const char *TAG = "main";
+
+void task_encoder(void *)
+{
+  // Subscribe this task to TWDT, then check if it is subscribed
+  ESP_ERROR_CHECK(esp_task_wdt_add(NULL));
+  ESP_ERROR_CHECK(esp_task_wdt_status(NULL));
+
   Encoder encoder(PIN_A, PIN_B, CPR);
 
-  while (true) {
+  while (true)
+  {
+    ESP_ERROR_CHECK(esp_task_wdt_reset());
+
     encoder.update();
-    printf("Position: %d\n", (int)encoder.get_position());
-    printf("Velocity: %f\n", encoder.get_velocity());
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    float position = encoder.get_position();
+    float velocity = encoder.get_velocity();
+
+    printf("%f, %f\n", position, velocity);
   }
+}
+
+extern "C" void app_main()
+{
+  xTaskCreatePinnedToCore(task_encoder, "task_encoder", 2048, NULL, 10, NULL, 0);
 }
